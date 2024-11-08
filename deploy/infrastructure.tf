4a5819c14e807c02142d6ad9b2e9b4c9bbc99979
syuhas <syuhas22@gmail.com>
2024-11-08 18:51:21 -0500

    removed references to php

--- deploy/infrastructure.tf
+++ deploy/infrastructure.tf
@@ -18,21 +18,21 @@ terraform {
 }
 
 # create ec2 instance
-resource "aws_instance" "s3-dashboard-php" {
+resource "aws_instance" "instance" {
   ami = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
   vpc_security_group_ids = [var.aws_security_group]
   key_name = "ec2"
   tags = {
-    Name = "application.${var.aws_subdomain}.${var.aws_domain}"
+    Name = "${var.aws_subdomain}.${var.aws_domain}"
   }
   iam_instance_profile = "instance_profile_role"
   subnet_id = element(var.aws_subnet_ids, 0)
 }
 
 # create target group for load balancer
-resource "aws_lb_target_group" "php-tg" {
-  name = "php-tg"
+resource "aws_lb_target_group" "target_group" {
+  name = "${var.aws_subdomain}.${var.aws_domain}-tg"
   port = 80
   protocol = "HTTP"
   vpc_id = var.aws_vpc_id
@@ -48,7 +48,7 @@ resource "aws_lb_target_group" "php-tg" {
 
 # create load balancer
 resource "aws_lb" "php-lb" {
-  name = "php-lb"
+  name = "${var.aws_subdomain}.${var.aws_domain}-lb"
   internal = false
   load_balancer_type = "application"
   security_groups = [var.aws_security_group]
@@ -56,26 +56,26 @@ resource "aws_lb" "php-lb" {
 }
 
 # create http listener for load balancer
-resource "aws_lb_listener" "php-http-lb-listener" {
+resource "aws_lb_listener" "http-lb-listener" {
   load_balancer_arn = aws_lb.php-lb.arn
   port = 80
   protocol = "HTTP"
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.php-tg.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
 
 # create https listener for load balancer
-resource "aws_lb_listener" "php-https-lb-listener" {
+resource "aws_lb_listener" "https-lb-listener" {
   load_balancer_arn = aws_lb.php-lb.arn
   port = 443
   protocol = "HTTPS"
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.php-tg.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 
   certificate_arn = var.aws_ssl_certificate_arn
@@ -83,13 +83,13 @@ resource "aws_lb_listener" "php-https-lb-listener" {
 }
 
 # register instance with the target group
-resource "aws_lb_target_group_attachment" "php-tg-attachment" {
-  target_group_arn = aws_lb_target_group.php-tg.arn
-  target_id = aws_instance.s3-dashboard-php.id
+resource "aws_lb_target_group_attachment" "target_group-attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id = aws_instance.instance.id
   port = 80
 }
 
-resource "aws_route53_record" "php-dns" {
+resource "aws_route53_record" "dns" {
   zone_id = var.aws_route53_zone_id
   name = "${var.aws_subdomain}.${var.aws_domain}"
   type = "A"
@@ -102,17 +102,17 @@ resource "aws_route53_record" "php-dns" {
 }
 
 output "instance_id" {
-  value = aws_instance.s3-dashboard-php.id
+  value = aws_instance.instance.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.s3-dashboard-php.public_ip
+  value = aws_instance.instance.public_ip
 }
 
 output "instance_public_dns" {
-  value = aws_instance.s3-dashboard-php.public_dns
+  value = aws_instance.instance.public_dns
 }
 
 output "instance_name" {
-  value = aws_instance.s3-dashboard-php.tags.Name  
+  value = aws_instance.instance.tags.Name  
 }

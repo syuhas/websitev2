1ae1e450540819013a871abbe3d06babddf7e565
syuhas <syuhas22@gmail.com>
2024-11-09 00:23:32 -0500

    trying file perms again

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,7 +14,7 @@ echo "Remote user: {$REMOTE_USER}"
 echo "Instance DNS: {$INSTANCE_PUBLIC_DNS}"
 
 sudo sed -i 's|server_name  tmp;.*|server_name  '"$INSTANCE_PUBLIC_DNS"';|' ./deploy/nginx.conf
-
+sudo chmod +x -R ./*
 echo "Copying project files to remote server..."
 scp -o StrictHostKeyChecking=no -r ./ "$REMOTE_USER@$INSTANCE_PUBLIC_DNS:/tmp"
 

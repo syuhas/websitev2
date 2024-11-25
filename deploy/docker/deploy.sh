d59c317296b8249d853aff8dd1b8cef5d08fa97b
syuhas <syuhas22@gmail.com>
2024-11-25 16:47:06 -0500

    add to the same network

--- deploy/docker/deploy.sh
+++ deploy/docker/deploy.sh
@@ -14,18 +14,32 @@ sudo chmod 777 /var/run/docker.sock
 
 cd /home/ec2-user/app
 
+# clean up any existing containers
 for container in $(docker ps -a -q); do
     echo "Stopping and removing container: $container"
     docker stop $container
     docker rm $container
 done
 
+# add a Docker network if it doesn't already exist
+NETWORK_NAME="my_network"
+if ! docker network ls | grep -q "$NETWORK_NAME"; then
+    echo "Creating Docker network: $NETWORK_NAME"
+    docker network create $NETWORK_NAME
+else
+    echo "Docker network $NETWORK_NAME already exists."
+fi
+
+
+# build and run website container
 docker build -t websitev2:latest .
 
-docker run -d -p 443:443 websitev2:latest
+docker run --network $NETWORK_NAME -d -p 443:443 websitev2:latest
 
 cd /home/ec2-user/app/api
 
+
+# build and run api container
 docker build -t api:latest .
 
 docker run -d -p 8000:8000 api:latest
@@ -36,6 +50,7 @@ sudo rm -rf app
 docker prune -f
 
 
+# check the status of the deployment
 echo "Checking running containers..."
 docker ps
 

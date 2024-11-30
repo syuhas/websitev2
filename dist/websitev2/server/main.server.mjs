fe82bbe951efd39f8eae6bc9d502e4b4ac066be3
syuhas <syuhas22@gmail.com>
2024-11-29 21:23:34 -0500

    new production bundle

--- dist/websitev2/server/main.server.mjs
+++ dist/websitev2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IOPLLTPQ.mjs";import"./chunk-CNKFVLIR.mjs";import"./chunk-ZFXCATAT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-B2CR622O.mjs";import"./chunk-CNKFVLIR.mjs";import"./chunk-ZFXCATAT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

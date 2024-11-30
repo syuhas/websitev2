86f1cd728dc76f391093391707b4418e9072d4b1
syuhas <syuhas22@gmail.com>
2024-11-29 20:49:29 -0500

    ephemeral didnt work

--- deploy/docker/infrastructure.tf
+++ deploy/docker/infrastructure.tf
@@ -3,10 +3,7 @@ provider "aws" {
 }
 
 variable "aws_security_group" {type = string}
-variable "aws_subnet_ids" {
-  type = list(string)
-  ephemeral = true
-}
+variable "aws_subnet_ids" {type = list(string)}
 variable "aws_tf_bucket" {type = string}
 variable "aws_route53_zone_id" {type = string}
 variable "aws_domain" {type = string}

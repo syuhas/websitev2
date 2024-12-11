d8f10b0aadf67e64303d8ae469261d983bbb6c47
syuhas <syuhas22@gmail.com>
2024-12-10 22:35:35 -0500

    fixed biiiiaaaaatch

--- dist/websitev2/server/main.server.mjs
+++ dist/websitev2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QMFLFKGT.mjs";import"./chunk-STC33EAI.mjs";import"./chunk-Z4E7CV4O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-KJGBKEPX.mjs";import"./chunk-STC33EAI.mjs";import"./chunk-Z4E7CV4O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

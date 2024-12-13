aa6f39c8f136ce2c2bdf09fe0871e99be598313d
syuhas <syuhas22@gmail.com>
2024-12-12 19:29:35 -0500

    .active for viewwidth

--- dist/websitev2/server/main.server.mjs
+++ dist/websitev2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RX4N72I3.mjs";import"./chunk-PYERTP3O.mjs";import"./chunk-6MJSRVEW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GTPJSFSS.mjs";import"./chunk-PYERTP3O.mjs";import"./chunk-6MJSRVEW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

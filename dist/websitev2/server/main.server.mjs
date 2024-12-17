c03aa033fc0b2d2c26df50ec3e746adc6b26a9de
syuhas <syuhas22@gmail.com>
2024-12-17 13:05:02 -0500

    one more homepage

--- dist/websitev2/server/main.server.mjs
+++ dist/websitev2/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-N2CXSRNZ.mjs";import"./chunk-JPOCARHA.mjs";import"./chunk-6MJSRVEW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YJKCKWG2.mjs";import"./chunk-JPOCARHA.mjs";import"./chunk-6MJSRVEW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
